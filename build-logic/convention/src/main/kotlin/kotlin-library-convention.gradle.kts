50a8b205ca61223ec38c6bce0888bcf6a934ffec
Adetunji Dahunsi <tjdah100@gmail.com>
2026-07-31 08:13:57 -0400

    Bump to AGP 9.2.1

--- build-logic/convention/src/main/kotlin/kotlin-library-convention.gradle.kts
+++ build-logic/convention/src/main/kotlin/kotlin-library-convention.gradle.kts
@@ -23,7 +23,7 @@ kotlin {
     applyDefaultHierarchyTemplate()
     androidLibrary {
         namespace = "com.tunjid.composables.${project.name}"
-        compileSdk = 36
+        compileSdk = 37
         minSdk = 23
     }
     jvm {
